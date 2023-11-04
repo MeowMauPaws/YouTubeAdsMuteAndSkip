ac5063a35e9e441ec777963a6e245ab4454e63a5
MeowMauPaws <146944545+MeowMauPaws@users.noreply.github.com>
2023-11-04 10:27:05 +0900

    ミュートが上手くいかない。たぶんisMutedが常にFalseになってる

--- src/content.js
+++ src/content.js
@@ -1,43 +1,50 @@
 // 以下のコードを content.js ファイルに保存します。
+let wasElementPresent = false;  // 変数を追加して、前回のチェック時に特定の要素が表示されていたかどうかを記録します。
 
 function checkForSpecificElement(selector) {
     return document.querySelector(selector) !== null;
 }
 
-function muteUnmute(mute) {
-    const muteButton = document.querySelector(".ytp-mute-button.ytp-button");
+function muteVideo() {
+    const muteButton = document.querySelector('.ytp-mute-button.ytp-button');
     if (muteButton) {
-        const isMuted =
-            muteButton.getAttribute("aria-label") ===
-            "ミュート（消音） キーボード ショートカット m";
-        if ((mute && !isMuted) || (!mute && isMuted)) {
+        const isMuted = muteButton.getAttribute('aria-label') === 'ミュート（消音） キーボード ショートカット m';
+        if (!isMuted) {
             muteButton.click();
         }
     }
 }
 
-function skipAdButton(on) {
-    const skipAdButton = document.querySelector(".ytp-ad-skip-button.ytp-button");
-    if (skipAdButton) {
-        const shouldSkipAdButtonClick =
-            skipAdButton.getAttribute("aria-pressed") === "true";
-        if ((on && !shouldSkipAdButtonClick) || (!on && shouldSkipAdButtonClick)) {
-            skipAdButton.click();
+function unmuteVideo() {
+    const muteButton = document.querySelector('.ytp-mute-button.ytp-button');
+    if (muteButton) {
+        const isMuted = muteButton.getAttribute('aria-label') === 'ミュート（消音） キーボード ショートカット m';
+        if (isMuted) {
+            muteButton.click();
         }
     }
 }
 
-const triggerSelector = ".ytp-ad-player-overlay-instream-info";
+function skipAdButton() {
+    const skipAdButton = document.querySelector(".ytp-ad-skip-button.ytp-button");
+    if (skipAdButton) {
+        skipAdButton.click();
+    }
+}
+
+const triggerSelector = ".ytp-ad-player-overlay";
 const adSkipTriggerSelector = ".ytp-ad-skip-button.ytp-button";
 
 setInterval(() => {
-    if (checkForSpecificElement(triggerSelector)) {
-        muteUnmute(true);
+    const isElementPresent = checkForSpecificElement(triggerSelector);  // 現在のチェック時に特定の要素が表示されているかどうかを確認します。
+    if (isElementPresent) {
+        muteVideo();
+    } else if (wasElementPresent) {  // 前回のチェック時に特定の要素が表示されていた場合、かつ現在表示されていない場合、ミュートを解除します。
+        unmuteVideo();
     }
+    wasElementPresent = isElementPresent;  // 前回のチェックの状態を更新します。
 
     if (checkForSpecificElement(adSkipTriggerSelector)) {
-        skipAdButton(true);
-    } else {
-        skipAdButton(false);
+        skipAdButton();
     }
-}, 1000);
+}, 100);
